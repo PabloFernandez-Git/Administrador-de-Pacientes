f1dd54766100561133711460acb4e66f0eed89a2
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-07-06 14:37:05 -0300

    Creando las Citas

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from "react";
 import uuid from 'uuid/dist/v4'
 
 
-const Formulario = () => {
+const Formulario = ({crearCita}) => {
 
     //Crear State de citas
     const [cita, actualizarCita] = useState({
@@ -43,6 +43,7 @@ const Formulario = () => {
         cita.id = uuid();
         
         // Crear la cita
+        crearCita(cita);
 
         // Reiniciar el form
     }
